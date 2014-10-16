c66c587ed3530bd85c11e39406371f6ad049a3b9
Nick McDonnough <nickmcd@gmail.com>
2014-10-16 10:45:45 -0500

    Refactor video routes to use resources routes helper.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Metube::Application.routes.draw do
   root to: "videos#index"
-  get "/videos", to: "videos#index"
-  get "/videos/new", to: "videos#new"
-  get "/videos/:id", to: "videos#show"
-  post "/videos" => "videos#create"
-  get "/videos/:id/edit", to: "videos#edit"
-  put "/videos/:id" => "videos#update"
-  delete "/videos/:id" => "videos#destroy"
+  resources :videos
+  # THE ABOVE LINE IS EQUIVALENT TO THE FOLLOWING
+  # get "/videos", to: "videos#index"
+  # get "/videos/new", to: "videos#new"
+  # get "/videos/:id", to: "videos#show"
+  # post "/videos" => "videos#create"
+  # get "/videos/:id/edit", to: "videos#edit"
+  # put "/videos/:id" => "videos#update"
+  # delete "/videos/:id" => "videos#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
